815a86853a8bb65a9ffaf82637e3496ead356aea
kPatch <irvsteve@gmail.com>
2020-06-19 07:08:19 -0400

    Add pre-commmit hook

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Pipe Viewer
 Rust piper visualizer
+
+
+## How to run
+
+```
+# Piping strings
+echo "a string" | cargo run
+```
+
+## Notices
+There's a ```pre-commit``` hook that runs fmt and clippy.
+
+```
+# Set environment variable
+echo "a string" | PV_SILENT=something cargo run
+```
+
+## Commands
+
+```
+# Format
+cargo fmt
+```
+
+```
+# Optimization suggestions
+cargo clippy
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::io::{self, Read, Write};
+
+const CHUNK_SIZE: usize = 16 * 1024; // sizes the buffer, 16 Kilobytes
+
 fn main() {
-    println!("Hello, world!");
+    if true == true {}
+    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
+    dbg!(silent);
+    let mut total_bytes = 0;
+    loop {
+        let mut buffer = [0; CHUNK_SIZE];
+        let num_read = match io::stdin().read(&mut buffer) {
+            // returns the number of bytes read
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break, // ignore the error, use placeholder '_'
+        };
+
+        dbg!(total_bytes += num_read);
+        io::stdout().write_all(&buffer[..num_read]).unwrap();
+    }
+    if dbg!(!silent) {
+        eprint!("num_read: {} ", total_bytes);
+    }
 }
